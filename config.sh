c76bc86a52e270d7abf55b701946185a2183650b
selocuba <124109654+selocuba@users.noreply.github.com>
2023-02-01 02:36:28 +0100

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Device
-export FOX_BRANCH="fox_9.0"
-export DT_LINK="https://gitlab.com/OrangeFox/device/beryllium.git -b fox_9.0"
+export FOX_BRANCH="fox_12.1"
+export DT_LINK="https://github.com/alg810/android_device_xiaomi_dandelion.git -b twrp-11"
 
-export DEVICE="beryllium"
+export DEVICE="dandelion"
 export OEM="xiaomi"
 
 # Build Target
